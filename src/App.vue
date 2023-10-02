af2d61ec80434372d66b06a3646deed9463c8532
Mannyg448 <manny.g448@hotmail.com>
2023-10-01 23:43:07 -0500

    Added Router

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-  import NavBar from '@/components/NavBar.vue'
-  import MainPage from '@/components/MainPage.vue'
+import NavBar from './components/NavBar.vue'
+
+
 </script>
 
+
+
+
 <template>
-  <NavBar />
-  <MainPage />
+    <NavBar />
+    <RouterView />
 </template>
