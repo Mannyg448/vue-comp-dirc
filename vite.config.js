e346e7ef3de3e2aa74792e469068480ff7a241f0
Mannyg448 <manny.g448@hotmail.com>
2023-10-08 23:59:23 -0500

    adding base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
+/** @type {import('vite').UserConfig} */
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-comp-dirc/'
   plugins: [vue()],
   resolve: {
     alias: {
